3296e6d0b6fdef4a8593b1df37edbd32e72facb5
Tarek <tarekttik.com>
2022-12-03 14:13:31 +0100

    email and password for student validation

--- backend/controllers/StudentController.js
+++ backend/controllers/StudentController.js
@@ -1,10 +1,21 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const validator = require("validator");
 const AsyncHandler = require("express-async-handler");
+
 const Student = require("../models/studentModel");
 
 const register = AsyncHandler(async (req, res) => {
   const { username, email, password, year } = req.body;
+  // checking if email is valid
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("L'email ou le mot de passe ne sont pas fournis");
+  }
+  if (!validator.isEmail(email)) {
+    throw new Error("L'email n'est pas valide");
+  }
+  // end of checking if email is valid
 
   const studentExist = await Student.findOne({ email });
 
@@ -17,12 +28,14 @@ const register = AsyncHandler(async (req, res) => {
   const hashPassword = await bcrypt.hash(password, salt);
 
   const student = await Student.create({
-    username: username,
-    email: email,
+    username,
+    email,
     password: hashPassword,
     year: year,
-    token: generateToken,
   });
+
+  // create token
+  const token = generateToken(student._id);
 
   if (student) {
     res.status(201).json({
@@ -30,7 +43,7 @@ const register = AsyncHandler(async (req, res) => {
       username: student.username,
       email: student.email,
       year: student.year,
-      token: generateToken(student._id),
+      token,
     });
   } else {
     res.status(401);
@@ -41,19 +54,31 @@ const register = AsyncHandler(async (req, res) => {
 const login = AsyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  const student = await Student.findOne({ email });
-
-  if (student && (await bcrypt.compare(password, student.password))) {
-    res.status(200).json({
-      username: student.username,
-      email: student.email,
-      year: student.year,
-      token: generateToken(student._id),
-    });
-  } else {
-    res.status(401);
-    throw new Error("Incorrect Data");
+  // validate if email and password are both provided
+  if (!email || !password) {
+    throw new Error("Email ou mot de passe manquant");
   }
+
+  const student = await Student.findOne({ email });
+  // validate if email is correct
+  if (!student) {
+    throw new Error("Adresse Email incorrecte");
+  }
+
+  // validate if password is correct
+  const match = await bcrypt.compare(password, student.password);
+  if (!match) {
+    throw new Error("Mot de passe incorrecte");
+  }
+
+  // student is logged in! create token and send it to the client along with student data
+
+  res.status(200).json({
+    username: student.username,
+    email: student.email,
+    year: student.year,
+    token: generateToken(student._id),
+  });
 });
 
 const deleteStudent = AsyncHandler(async (req, res) => {
