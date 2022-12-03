117fbeecd5f642a4a9df2b432c96ae62d60b3ec8
oussama <otaksam213@gmail.com>
2022-12-03 17:49:39 +0100

    DWM API

--- backend/controllers/subjectController.js
+++ backend/controllers/subjectController.js
@@ -1,1 +1,19 @@
-    
+const asyncHandler = require("express-async-handler");
+const Subject = require("../models/subjectModel");
+
+const setSubject = asyncHandler(async (req, res) => {
+  const subject = await Subject.create({
+    name: req.body.name,
+    profId: req.prof.id,
+    year: req.body.year,
+  });
+
+  if (subject) {
+    res.status(201).json({ subject });
+  } else {
+    res.status(401);
+    throw new Error("Somthing is wrong");
+  }
+});
+
+module.exports = { setSubject };
--- backend/routes/profRoute.js
+++ backend/routes/profRoute.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const { insertMarks } = require("../controllers/marksController");
 const route = express.Router();
 const { register, login } = require("../controllers/ProfController");
-
+const { setSubject } = require("../controllers/subjectController");
+const { protect } = require("../middleware/authMiddleware");
 route.post("/", register).post("/login", login);
+route.post("/subject", protect, setSubject);
+route.post("/marks", protect, insertMarks);
 
 module.exports = route;
