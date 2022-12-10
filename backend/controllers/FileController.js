a623b5e593a0b27854d8a0c208e450363f76d006
oussamachah2020 <otaksam213@gmail.com>
2022-12-10 19:40:56 +0100

    new file changes

--- backend/controllers/FileController.js
+++ backend/controllers/FileController.js
@@ -2,6 +2,7 @@ const multer = require("multer");
 const FileModel = require("../models/fileModel");
 const fsPromises = require("fs/promises");
 const AsyncHandler = require("express-async-handler");
+const fs = require("fs");
 
 //Cours upload
 const Cours = multer.diskStorage({
@@ -42,8 +43,9 @@ const uploadTDs = multer({
 const deleteFile = AsyncHandler(async (req, res) => {
   const { fileId } = req.params;
   const File = await FileModel.findOneAndRemove({ _id: fileId });
+  // const pdf = fs.writeFile(File, File.data).toString('utf8')
   if (File) {
-    res.status(200).json("deleted successefuly");
+    res.status(200).json("deleted successfully");
   } else {
     res.status(400);
     throw new Error("Something wrong or wrong path");
@@ -52,17 +54,21 @@ const deleteFile = AsyncHandler(async (req, res) => {
 
 const getFiles = AsyncHandler(async (req, res) => {
   const files = await FileModel.find({ profID: req.prof.id });
+
   if (files) {
+    files.forEach(file => {
+
+    })
     res.status(200).json({ files });
-    if (files.Filetype == "Cours") {
+    if (files.Filetype === "Cours") {
       await fs.promises.access("uploads/Cours");
     }
 
-    if (files.Filetype == "TP") {
+    if (files.Filetype === "TP") {
       await fs.promises.access("uploads/TPs");
     }
 
-    if (files.Filetype == "TD") {
+    if (files.Filetype === "TD") {
       await fs.promises.access("uploads/TDs");
     }
   }
